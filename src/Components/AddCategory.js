29fea81173eff78600f115f88f55cc578ecffb37
mgelabertr <mgelabertr@yahoo.es>
2022-01-05 14:56:58 +0100

    prueba

--- src/Components/AddCategory.js
+++ src/Components/AddCategory.js
@@ -14,6 +14,7 @@ export const AddCategory = ( {setCategories} ) => {
         e.preventDefault();
 
         if(inputValue.trim().length > 2){
+            
             console.log("submit");
         
             setCategories( cats => [ inputValue, ...cats]);
